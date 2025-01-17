db23af734d521e2ef311cbf8c392d3a412d39e13
Justine <paralejasjustine15@gmail.com>
2025-01-18 00:44:29 +0800

    Small fix

--- cmd/services/service/main.go
+++ cmd/services/service/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/service/api", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/service/api/*", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Request URL: ", r.URL)
 		fmt.Fprintf(w, "Hello from service")
 	})
--- cmd/services/service_1/main.go
+++ cmd/services/service_1/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/service-1/api", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/service-1/api/*", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Request URL path: ", r.URL.Path)
 		fmt.Fprintf(w, "Hello from service-1")
 	})
