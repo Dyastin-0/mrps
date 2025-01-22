a0b09080bac2b48969468d2f5f59b1c021d52e87
Justine <paralejasjustine15@gmail.com>
2025-01-23 00:21:24 +0800

    add log

--- internal/reverseproxy/reverse_proxy.go
+++ internal/reverseproxy/reverse_proxy.go
@@ -1,6 +1,7 @@
 package reverseproxy
 
 import (
+	"log"
 	"net/http"
 	"strings"
 
@@ -12,10 +13,13 @@ func Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		host := strings.ToLower(r.Host)
 		path := r.URL.Path
-
+		log.Println("[DEBUG] ====================================")
 		if domainConfig, exists := config.Routes[host]; exists {
 			for routePath, proxyTarget := range domainConfig.Routes {
+				log.Println("[DEBUG] Current path", routePath)
 				if strings.HasPrefix(path, routePath) {
+					log.Println("[DEBUG] matched path", routePath, "Request path", path)
+					log.Println("[DEBUG] Proxying request to", proxyTarget)
 					reverseproxy.New(proxyTarget).ServeHTTP(w, r)
 					return
 				}
