c0074842203cc327db66a442a70926f7a9ade51b
Justine <paralejasjustine15@gmail.com>
2025-01-22 22:59:28 +0800

    Small fix

--- internal/reverseproxy/reverse_proxy.go
+++ internal/reverseproxy/reverse_proxy.go
@@ -10,10 +10,6 @@ import (
 
 func Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate")
-		w.Header().Set("Pragma", "no-cache")
-		w.Header().Set("Expires", "0")
-
 		host := strings.ToLower(r.Host)
 		path := r.URL.Path
 
