59a48f5d295db64baaa5c23916d1991cc99c0abf
Justine <paralejasjustine15@gmail.com>
2025-01-25 13:51:44 +0800

    fix host

--- internal/allowedhost/allowedhost.go
+++ internal/allowedhost/allowedhost.go
@@ -1,7 +1,6 @@
 package allowedhost
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/Dyastin-0/mrps/internal/config"
@@ -9,10 +8,9 @@ import (
 
 func Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		host := r.URL.Host
+		host := r.Host
 
 		if config.DomainTrie.Match(host) == nil {
-			fmt.Println(host)
 			http.Error(w, "forbidden, host not allowed", http.StatusForbidden)
 			return
 		}
