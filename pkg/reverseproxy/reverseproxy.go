dd14d64725c09ba4dbcc6be7491cbac9f99f2c43
Justine <paralejasjustine15@gmail.com>
2025-01-28 00:36:43 +0800

    add transport and headers

--- pkg/reverseproxy/reverseproxy.go
+++ pkg/reverseproxy/reverseproxy.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 
 	"github.com/Dyastin-0/mrps/internal/config"
 	"github.com/Dyastin-0/mrps/pkg/rewriter"
@@ -12,11 +13,20 @@ import (
 
 func New(target string, path string) http.Handler {
 	targetURL, err := url.Parse(target)
+
 	if err != nil {
 		log.Fatalf("Failed to parse target URL: %v", err)
 	}
 
+	transport := &http.Transport{
+		MaxIdleConns:        10,
+		MaxIdleConnsPerHost: 5,
+		IdleConnTimeout:     30 * time.Second,
+		DisableKeepAlives:   false,
+	}
+
 	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+	proxy.Transport = transport
 	proxy.Director = func(req *http.Request) {
 		req.URL.Scheme = targetURL.Scheme
 		req.URL.Host = targetURL.Host
@@ -28,6 +38,12 @@ func New(target string, path string) http.Handler {
 		req.URL.Path = rewrittenPath
 
 		req.Host = targetURL.Host
+
+		req.Header.Set("X-Forwared-For", req.RemoteAddr)
+		req.Header.Set("X-Forwarded-Proto", "https")
+		req.Header.Set("X-Forwarded-Host", req.Host)
+		req.Header.Set("X-Real-IP", req.RemoteAddr)
+		req.Header.Set("Connection", "keep-alive")
 	}
 
 	return proxy
