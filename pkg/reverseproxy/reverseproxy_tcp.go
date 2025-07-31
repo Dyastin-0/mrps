7db2214ac3dbcee04f48d247e932be8ca5ea5d22
Justine Paralejas <paralejasjustine15@gmail.com>
2025-07-31 16:50:56 +0800

    fix

--- pkg/reverseproxy/reverseproxy_tcp.go
+++ pkg/reverseproxy/reverseproxy_tcp.go
@@ -1,7 +1,6 @@
 package reverseproxy
 
 import (
-	"crypto/tls"
 	"io"
 	"net"
 	"sync"
@@ -12,47 +11,6 @@ type TCPProxy struct {
 	// optional for tls
 	WithTLS    bool
 	ServerName string
-}
-
-func (t *TCPProxy) ForwardTLS(src net.Conn) error {
-	tlsconfig := tls.Config{
-		ServerName: t.ServerName,
-	}
-	dst, err := tls.Dial("tcp", t.Addr, &tlsconfig)
-	if err != nil {
-		src.Close()
-		return err
-	}
-
-	defer func() {
-		src.Close()
-		dst.Close()
-	}()
-
-	errch := make(chan error, 2)
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go func() {
-		errch <- t.stream(src, dst)
-		wg.Done()
-	}()
-
-	go func() {
-		errch <- t.stream(src, dst)
-		wg.Done()
-	}()
-
-	wg.Wait()
-	close(errch)
-
-	for err := range errch {
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 func (t *TCPProxy) Forward(src net.Conn) error {
@@ -72,17 +30,17 @@ func (t *TCPProxy) Forward(src net.Conn) error {
 	wg.Add(2)
 
 	go func() {
-		errch <- t.stream(src, dst)
-		wg.Done()
+		t.stream(&wg, src, dst, errch)
 	}()
 
 	go func() {
-		errch <- t.stream(src, dst)
-		wg.Done()
+		t.stream(&wg, dst, src, errch)
 	}()
 
-	wg.Wait()
-	close(errch)
+	go func() {
+		wg.Wait()
+		close(errch)
+	}()
 
 	for err := range errch {
 		if err != nil {
@@ -93,15 +51,15 @@ func (t *TCPProxy) Forward(src net.Conn) error {
 	return nil
 }
 
-func (t *TCPProxy) stream(src, dst net.Conn) error {
+func (t *TCPProxy) stream(wg *sync.WaitGroup, src, dst net.Conn, errch chan error) {
+	defer wg.Done()
+
 	_, err := io.Copy(dst, src)
 	if err != nil {
-		return err
+		errch <- err
 	}
 
 	if conn, ok := dst.(*net.TCPConn); ok {
 		conn.CloseWrite()
 	}
-
-	return nil
 }
