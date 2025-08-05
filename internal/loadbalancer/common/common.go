377abdd3ccb1d1e92c068b96ac5bdc67129b6ca0
Justine Paralejas <paralejasjustine15@gmail.com>
2025-08-05 23:31:18 +0800

    fix: remove unneeded health logging

--- internal/loadbalancer/common/common.go
+++ internal/loadbalancer/common/common.go
@@ -62,23 +62,19 @@ func (d *Dest) CheckTCP(ctx context.Context, host string, delay time.Duration) {
 func (d *Dest) pingTCP(host string) {
 	conn, err := net.DialTimeout("tcp", host, time.Second)
 	if err != nil {
-		log.Warn().Str("host", host).Str("url", d.URL).Str("proto", "tcp").Str("status", "down").Msg("health")
 		d.Alive = false
 	} else {
 		conn.Close()
 		d.Alive = true
-		log.Warn().Str("host", host).Str("url", d.URL).Str("proto", "tcp").Str("status", "up").Msg("health")
 	}
 }
 
 func (d *Dest) ping(host string) {
 	resp, err := httpClient.Get(d.URL)
 	if err != nil {
-		log.Warn().Str("host", host).Str("url", d.URL).Str("proto", "http").Str("status", "down").Msg("health")
 		d.Alive = false
 	} else {
 		resp.Body.Close()
-		log.Info().Str("host", host).Str("url", d.URL).Str("proto", "http").Str("status", "up").Msg("health")
 		d.Alive = true
 	}
 }
